e966e2d3b42ac482612dfc31ba7c972e598183ab
Tomas <timkanders@gmail.com>
2019-10-29 15:50:15 +0000

    Changed Sales file counter
    Now increments on creation and uses new increment as id

--- CPP_2019_CA2_TS_AR/Sale.cpp
+++ CPP_2019_CA2_TS_AR/Sale.cpp
@@ -2,12 +2,16 @@
 int Sale::salesCount = 50;
 Sale::Sale()
 {
-
+	salesCount++;
+	setID(salesCount);
+	setAssistant("George Default");
+	//setItems();
 }
 
 Sale::Sale(string salesAssistant, list<StockItem> items)
 {
-	setID(id);
+	salesCount++;
+	setID(salesCount);
 	setAssistant(salesAssistant);
 	setItems(items);
 }
@@ -16,29 +20,44 @@ Sale::~Sale()
 {
 }
 
+//Getters
 int Sale::getID()
 {
-	return 0;
+	return this->ID;
 }
-
 string Sale::getAssistant()
 {
 	return string();
 }
-
 list<StockItem> Sale::getItems()
 {
 	return list<StockItem>();
 }
 
+
 void Sale::setID(int ID)
 {
+	this->ID = ID;
 }
-
 void Sale::setAssistant(string assistant)
 {
+	if (assistant.length() <4)
+	{
+		throw domain_error("Not Valid Length");
+	}
+	else
+	{
+		this->salesAssistant = assistant;
+	}
 }
-
 void Sale::setItems(list<StockItem> items)
 {
+	if (items.size() <1)
+	{
+		throw domain_error("Need More Items");
+	}
+	else
+	{
+		this->items = items;
+	}
 }
