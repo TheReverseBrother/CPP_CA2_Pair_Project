bdfbc8d8c34796918ecaec8dd2325e7379de332d
Tomas <timkanders@gmail.com>
2019-10-29 15:27:35 +0000

    Added Function Definitions for sales Class

--- CPP_2019_CA2_TS_AR/Sale.cpp
+++ CPP_2019_CA2_TS_AR/Sale.cpp
@@ -1,1 +1,44 @@
 #include "Sale.h"
+int Sale::salesCount = 50;
+Sale::Sale()
+{
+
+}
+
+Sale::Sale(string salesAssistant, list<StockItem> items)
+{
+	setID(id);
+	setAssistant(salesAssistant);
+	setItems(items);
+}
+
+Sale::~Sale()
+{
+}
+
+int Sale::getID()
+{
+	return 0;
+}
+
+string Sale::getAssistant()
+{
+	return string();
+}
+
+list<StockItem> Sale::getItems()
+{
+	return list<StockItem>();
+}
+
+void Sale::setID(int ID)
+{
+}
+
+void Sale::setAssistant(string assistant)
+{
+}
+
+void Sale::setItems(list<StockItem> items)
+{
+}
