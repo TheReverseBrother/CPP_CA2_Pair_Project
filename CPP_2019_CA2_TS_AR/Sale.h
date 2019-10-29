7a539e99abe27f8bd0f7866930e334e9de167737
Tomas <timkanders@gmail.com>
2019-10-29 15:11:00 +0000

    Added Function Declarations in Sales Header
    Added all declarations for Sales Class

--- CPP_2019_CA2_TS_AR/Sale.h
+++ CPP_2019_CA2_TS_AR/Sale.h
@@ -1,5 +1,29 @@
 #pragma once
+#include "pch.h"
+#include <list>
 class Sale
 {
+private:
+	int ID;
+	string salesAssistant;
+	list<StockItem> items;
+	static int salesCount;
+
+public:
+	Sale();
+	Sale(int id, string salesAssistant, list<StockItem> items);
+	~Sale();
+
+	//Getters
+	int getID();
+	string getAssistant();
+	list<StockItem> getItems();
+
+	//Setters
+	void setID(int ID);
+	void setAssistant(string assistant);
+	void setItems(list<StockItem> items);
+
+
 };
 
