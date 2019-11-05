3631dc5fe98e8ee16719ff3b9fafd73e999e8c4e
Tomas <timkanders@gmail.com>
2019-11-05 13:00:40 +0000

    Added function Definitions

--- CPP_2019_CA2_TS_AR/SalesAnalysis.cpp
+++ CPP_2019_CA2_TS_AR/SalesAnalysis.cpp
@@ -1,2 +1,91 @@
-
 #include "SalesAnalysis.h"
+
+int SalesAnalysis::SalesCount = 1661;
+
+SalesAnalysis::SalesAnalysis()
+{
+	this->ID = 1;
+	this->dayOfCreation = time(0);
+	this->lastAnalysis = time(0);
+	this->totalValueOfSales = 1.00;
+}
+
+SalesAnalysis::SalesAnalysis(int ID, time_t dayOfCreation, time_t lastAnalysis, float totalValueOfSales)
+{
+	setID(ID);
+	setDateOfCreation(dayOfCreation);
+	setLastAnalysis(lastAnalysis);
+	setTotalValue(totalValueOfSales);
+}
+
+SalesAnalysis::SalesAnalysis(time_t lastAnalysis, float totalValueOfSales)
+{
+	++SalesAnalysis::SalesCount;
+	setID(SalesAnalysis::SalesCount);
+	this->dayOfCreation = time(0);
+	setLastAnalysis(lastAnalysis);
+	setTotalValue(totalValueOfSales);
+}
+
+SalesAnalysis::~SalesAnalysis()
+{
+}
+
+
+//Getters
+time_t SalesAnalysis::getDateOfCreation() const
+{
+	return this->dayOfCreation;
+}
+
+time_t SalesAnalysis::getlastAnalysis() const
+{
+	return this->lastAnalysis;
+}
+
+float SalesAnalysis::getTotalValue() const
+{
+	return this->totalValueOfSales;
+}
+
+
+//Setters
+void SalesAnalysis::setID(int& ID)
+{
+	this->ID = ID;
+}
+
+void SalesAnalysis::setLastAnalysis(time_t& LA)
+{
+	this->lastAnalysis = LA;
+}
+
+void SalesAnalysis::setDateOfCreation(time_t& DOC)
+{
+	this->dayOfCreation = DOC;
+}
+
+void SalesAnalysis::setTotalValue(float& value)
+{
+	this->totalValueOfSales = value;
+}
+
+ostream& operator<<(ostream& os, const SalesAnalysis& saleAnalysis)
+{
+	// TODO: insert return statement here
+}
+
+istream& operator>>(istream& in, const SalesAnalysis& saleAnalysis)
+{
+	// TODO: insert return statement here
+}
+
+list<SalesAnalysis> SalesAnalysis::loadAnalysises()
+{
+	return list<SalesAnalysis>();
+}
+
+void SalesAnalysis::saveAnalysises()
+{
+}
+
--- CPP_2019_CA2_TS_AR/SalesAnalysis.h
+++ CPP_2019_CA2_TS_AR/SalesAnalysis.h
@@ -4,6 +4,29 @@ class SalesAnalysis
 {
 private:
 	int ID;
-	time_t dayOfCreation;
-	time_t lastAnalysis;
+	time_t dayOfCreation, lastAnalysis;
+	float totalValueOfSales;
+	static int SalesCount;
+public:
+	SalesAnalysis();
+	SalesAnalysis(int ID, time_t dayOfCreation, time_t lastAnalysis, float totalValueOfSales);
+	SalesAnalysis(time_t lastAnalysis, float totalValueOfSales);
+	~SalesAnalysis();
+
+	time_t getDateOfCreation() const;
+	time_t getlastAnalysis() const;
+	float getTotalValue() const;
+
+	void setID(int ID);
+	void setLastAnalysis(time_t LA);
+	void setDateOfCreation(time_t DOC);
+	void setTotalValue(float value);
+
+	friend ostream& operator<<(ostream& os, const SalesAnalysis& saleAnalysis);
+	friend istream& operator>>(istream& in, const SalesAnalysis& saleAnalysis);
+
+
+	static list<SalesAnalysis> loadAnalysises();
+	static void saveAnalysises();
+
 };
