431fb071874b0d76901c0dab6b43629df62983ad
Tomas <timkanders@gmail.com>
2019-11-05 19:54:40 +0000

    Added Read in And Out For Sale Analysis

--- CPP_2019_CA2_TS_AR/SalesAnalysis.cpp
+++ CPP_2019_CA2_TS_AR/SalesAnalysis.cpp
@@ -1,29 +1,27 @@
 #include "SalesAnalysis.h"
-
+#include <list>
 int SalesAnalysis::SalesCount = 1661;
 
 SalesAnalysis::SalesAnalysis()
 {
 	this->ID = 1;
 	this->dayOfCreation = time(0);
-	this->lastAnalysis = time(0);
 	this->totalValueOfSales = 1.00;
 }
 
-SalesAnalysis::SalesAnalysis(int ID, time_t dayOfCreation, time_t lastAnalysis, float totalValueOfSales)
+SalesAnalysis::SalesAnalysis(int ID, time_t dayOfCreation, float totalValueOfSales)
 {
 	setID(ID);
 	setDateOfCreation(dayOfCreation);
-	setLastAnalysis(lastAnalysis);
+	
 	setTotalValue(totalValueOfSales);
 }
 
-SalesAnalysis::SalesAnalysis(time_t lastAnalysis, float totalValueOfSales)
+SalesAnalysis::SalesAnalysis(float totalValueOfSales)
 {
 	++SalesAnalysis::SalesCount;
 	setID(SalesAnalysis::SalesCount);
 	this->dayOfCreation = time(0);
-	setLastAnalysis(lastAnalysis);
 	setTotalValue(totalValueOfSales);
 }
 
@@ -31,18 +29,15 @@ SalesAnalysis::~SalesAnalysis()
 {
 }
 
-
 //Getters
+int SalesAnalysis::getID() const
+{
+	return this->ID;
+}
 time_t SalesAnalysis::getDateOfCreation() const
 {
 	return this->dayOfCreation;
 }
-
-time_t SalesAnalysis::getlastAnalysis() const
-{
-	return this->lastAnalysis;
-}
-
 float SalesAnalysis::getTotalValue() const
 {
 	return this->totalValueOfSales;
@@ -53,11 +48,6 @@ float SalesAnalysis::getTotalValue() const
 void SalesAnalysis::setID(int& ID)
 {
 	this->ID = ID;
-}
-
-void SalesAnalysis::setLastAnalysis(time_t& LA)
-{
-	this->lastAnalysis = LA;
 }
 
 void SalesAnalysis::setDateOfCreation(time_t& DOC)
@@ -72,20 +62,92 @@ void SalesAnalysis::setTotalValue(float& value)
 
 ostream& operator<<(ostream& os, const SalesAnalysis& saleAnalysis)
 {
-	// TODO: insert return statement here
+	os << saleAnalysis.getID() << "/" << saleAnalysis.getDateOfCreation() << "/" <<  saleAnalysis.getTotalValue() << endl;
+
+	return os;
 }
 
-istream& operator>>(istream& in, const SalesAnalysis& saleAnalysis)
+istream& operator>>(istream& in,SalesAnalysis& saleAnalysis)
 {
-	// TODO: insert return statement here
+	int id,timeInt = 0;
+	float price;
+	time_t timeInfo;
+	string data,info;
+	string delimiter = "/";
+	getline(in,data);
+	if (data.empty())
+	{
+		return in;
+	}
+	try
+	{
+		info = data.substr(0, data.find(delimiter));
+		data.erase(0, data.find(delimiter) + delimiter.length());
+		id = stoi(info);
+		saleAnalysis.setID(id);
+
+		info = data.substr(0, data.find(delimiter));
+		data.erase(0, data.find(delimiter) + delimiter.length());
+		timeInt = stoi(info);
+		timeInfo = (time_t) timeInt;
+		saleAnalysis.setDateOfCreation(timeInfo);
+
+		info = data.substr(0, data.find(delimiter));
+		data.erase(0, data.find(delimiter) + delimiter.length());
+	}
+	catch (exception e)
+	{
+		e.what();
+		int x = 0;
+		saleAnalysis.setID(x);
+	}
+
+	return in;
+
 }
 
 list<SalesAnalysis> SalesAnalysis::loadAnalysises()
 {
-	return list<SalesAnalysis>();
+	list<SalesAnalysis> analysises;
+	ifstream in;
+	in.open("sales-analysis.txt");
+	if (in.fail())
+	{
+		cout << "Failed To Load Sales Analysis" << endl;
+	}
+	else
+	{
+		while (!in.eof())
+		{
+			SalesAnalysis saleAnlysis;
+			in >> saleAnlysis;
+			if (saleAnlysis.getID() != 0)
+			{
+				analysises.push_back(saleAnlysis);
+			}
+		}
+	}
+	return analysises;
 }
 
-void SalesAnalysis::saveAnalysises()
+void SalesAnalysis::saveAnalysises(list<SalesAnalysis>& list)
 {
+	ofstream out;
+	out.open("sales-analysis.txt");
+	if (out.fail())
+	{
+		cout << "Error Saving SalesAnalysis" << endl;
+		out.close();
+	}
+	else
+	{
+
+		for (SalesAnalysis s : list)
+		{
+			out << s << endl;
+		}
+
+	}
+	out.close();
 }
 
--- CPP_2019_CA2_TS_AR/SalesAnalysis.h
+++ CPP_2019_CA2_TS_AR/SalesAnalysis.h
@@ -1,32 +1,32 @@
+#pragma once
 #include "pch.h"
 
 class SalesAnalysis
 {
 private:
 	int ID;
-	time_t dayOfCreation, lastAnalysis;
+	time_t dayOfCreation;
 	float totalValueOfSales;
 	static int SalesCount;
 public:
 	SalesAnalysis();
-	SalesAnalysis(int ID, time_t dayOfCreation, time_t lastAnalysis, float totalValueOfSales);
-	SalesAnalysis(time_t lastAnalysis, float totalValueOfSales);
+	SalesAnalysis(int ID, time_t dayOfCreation,float totalValueOfSales);
+	SalesAnalysis(float totalValueOfSales);
 	~SalesAnalysis();
 
+	int getID() const;
 	time_t getDateOfCreation() const;
-	time_t getlastAnalysis() const;
 	float getTotalValue() const;
 
-	void setID(int ID);
-	void setLastAnalysis(time_t LA);
-	void setDateOfCreation(time_t DOC);
-	void setTotalValue(float value);
+	void setID(int& ID);
+	void setDateOfCreation(time_t& DOC);
+	void setTotalValue(float& value);
 
 	friend ostream& operator<<(ostream& os, const SalesAnalysis& saleAnalysis);
 	friend istream& operator>>(istream& in, const SalesAnalysis& saleAnalysis);
 
 
 	static list<SalesAnalysis> loadAnalysises();
-	static void saveAnalysises();
+	static void saveAnalysises(list<SalesAnalysis>& list);
 
 };
