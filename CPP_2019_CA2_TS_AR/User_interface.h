923d4d47152303956f872ceda87667ad9383b4f8
Unknown <d00212721@student.dkit.ie>
2019-11-09 12:35:16 +0000

    Added main menu

--- CPP_2019_CA2_TS_AR/User_interface.h
+++ CPP_2019_CA2_TS_AR/User_interface.h
@@ -1,6 +1,11 @@
 #pragma once
 
-void test() 
+void mainMenu() 
 {
-	cout << "ui test" << endl;
+	cout << "1: Add Stock" << endl;
+	cout << "2: Add Sale" << endl;
+	cout << "3: Analyse Sales" << endl;
+	cout << "4: Search Stock" << endl;
+	cout << "5: Remove Item" << endl;
+	cout << "6: Quit" << endl;
 }
