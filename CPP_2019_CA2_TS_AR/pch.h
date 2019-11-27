3ec81e3ebcc8759482270d01d81f3627b7b15aa6
Tomas <timkanders@gmail.com>
2019-11-27 15:22:50 +0000

    Added time to string Function

--- CPP_2019_CA2_TS_AR/pch.h
+++ CPP_2019_CA2_TS_AR/pch.h
@@ -11,3 +11,14 @@
 #include "Sale.h"
 enum class SIZE { XS, S, M, L, XL, XXL, XXXL };
 using namespace std;
+
+static string time_to_local_date(time_t& utc)
+{
+	struct tm newtime;
+	time_t now = time(0);
+	localtime_s(&newtime, &now);
+
+	string date = to_string(newtime.tm_mday) + "/" + to_string(newtime.tm_mon) + "/" + to_string(1900 + newtime.tm_year);
+
+	return date;
+}
